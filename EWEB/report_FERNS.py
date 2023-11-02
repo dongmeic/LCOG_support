42b89346285a993dc105fc3141b84c37baeb768d
dongmeic <dongmeic@uoregon.edu>
2023-11-01 17:36:34 -0700

    updated the FERNS report

--- EWEB/report_FERNS.py
+++ EWEB/report_FERNS.py
@@ -1,4 +1,5 @@
-import win32com.client as win32
+import win32com.client
+from win32com.client import Dispatch, constants
 import geopandas as gpd
 import pandas as pd
 import sys
@@ -12,11 +13,23 @@ print(f"Current update includes {data.shape[0]} records and below shows the summ
 print(data.groupby('ActType').agg(SumAcres = pd.NamedAgg(column = 'SUM_calc_acres', aggfunc = sum)))
 print("\n")
 
-outlook = win32.Dispatch('outlook.application')
-mail = outlook.CreateItem(0)
-mail.To = 'dchen@lcog.org'
-mail.Subject = f'Complete FERNS update {str(today)}'
-mail.Body = inpath+'\\FERNS_log.txt'
-mail.Send()
+const=win32com.client.constants
+olMailItem = 0x0
+obj = win32com.client.Dispatch("Outlook.Application")
+newMail = obj.CreateItem(olMailItem)
+newMail.To = 'CBARROWS@Lcog.org'
+#newMail.To = 'DCHEN@Lcog.org'
+newMail.Subject = f'Complete FERNS update {str(today)}'
+#newMail.Subject = 'test'
+newMail.BodyFormat = 2
+newMail.HTMLBody = f"""
+<HTML><BODY><p>Hi Chrissy,</p> 
+
+This is an auto-generated email from updating the FERNS dashboard. The program running log is located at {inpath}\\FERNS_log.txt. This email notifies you that the update has been completed automatically, and you only need to edit the date info on the <a href='https://lcog.maps.arcgis.com/apps/dashboards/f003689bcb7f45eca5be6f02baada6c0#mode=edit'>dashboard</a>. </p> 
+
+<p>Have a great day!</p></BODY></HTML>
+
+"""
+newMail.Send()
 print("Sent out the email...")
 sys.stdout.close()
